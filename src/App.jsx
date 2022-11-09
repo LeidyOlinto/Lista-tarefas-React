1393591a2011fa5b90c478a05c8aed1c23b854cd
leidyolinto <suverleide.olinto@gmail.com>
2022-11-09 16:07:26 -0300

    projeto em react

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';//Gera um Id aleatorio.
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css';
+import './index.css';
+import Tasks from "./components/Tasks"
+import AddTask from './components/AddTask'
+import Header from './components/Header';
+import TaskDetails from './components/TaskDetails';
+import axios from 'axios';
+
+
+
+const App = () => {
+  //let const message= "Hello World!!";
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar progrogramação',
+      completed: false,
+    },
+    {
+      id: '2',
+      title: 'Ler Livros',
+      completed: true,
+    }
+  ]);
+  //API 
+useEffect(() => {
+  const fetchTasks = async () =>{
+    const {data} = await axios.get(
+      "https://jsonplaceholder.cypress.io/todos?_limit=10"
+    );
+    setTasks(data);
+  };
+  fetchTasks();
+},[])
+
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) return { ...task, completed: !task.completed };
+      return task;
+    })
+    setTasks(newTasks);
+  };
+  const handleTaskAddition = (taskTitle) => {
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),//Gera um Id aleatorio.
+        completed: false,
+      },
+    ];
+    setTasks(newTasks)
+  };
+  //Remover tarefa.
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id !== taskId);
+
+    setTasks(newTasks);
+  };
+
+  return (
+    //Aqui renderiza na tela 
+    <Router>
+      <div className='container'>
+        <Header />
+
+        <Routes>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <>
+                <AddTask handleTaskAddition={handleTaskAddition} />
+                <Tasks
+                  tasks={tasks}
+                  handleTaskClick={handleTaskClick}
+                  handleTaskDeletion={handleTaskDeletion}
+
+                />
+              </>
+            )}
+          />
+          <Route path="/:taskTile" exact component={TaskDetails} />
+
+        </Routes>
+
+      </div>
+    </Router>
+
+  )
+};
+export default App;
---
+++ src/components/Task.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { CgClose, CgInfo } from "react-icons/cg";
+import {BrowserRouter as useHistory} from "react-router-dom";
+import './Task.css';
+
+const Task = ({ task, handleTaskClick, handleTaskDeletion }) => {
+    const history= useHistory();
+
+    const handleTaskDetailsClick = ()=>{
+        history.push(`/${task.title}`)
+    }
+    return (
+        <div className="task-container"
+            style={task.completed ? { borderLeft: "6px solid chartreuse" } : {}}
+        >
+            <div className="task-title" onClick={() => handleTaskClick(task.id)}>
+                {task.title}
+            </div>
+            <div className="buttons-conatainer">
+                <button className="remove-task-button"
+                    onClick={() => handleTaskDeletion(task.id)}
+                ><CgClose />
+                </button>
+                <button className="see-task-details-button"
+                onClick={handleTaskDetailsClick}
+                >   
+                    <CgInfo />
+                </button>
+
+            </div>
+        </div>
+    );
+};
+
+export default Task;
